840f366aaf0743d4fbc5d76327073295ed60de31
Дмитрий Смирнов <dsmirnov@ebi.ac.uk>
2018-10-02 13:19:02 +0100

    minor changes

--- src/test/java/net/sf/cram/Cram2FastqTest.java
+++ src/test/java/net/sf/cram/Cram2FastqTest.java
@@ -6,9 +6,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Enumeration;
 
 import org.junit.Assert;
 import org.junit.Test;
